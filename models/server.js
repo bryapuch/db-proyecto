85c4a30f3ba74821a39fc1f6dac6591399b6ceb6
bryan puchaicela <bryapuch@espol.edu.ec>
2021-11-14 02:01:39 -0500

    add new partials how cards,nav,main

--- models/server.js
+++ models/server.js
@@ -1,21 +1,25 @@
 const express = require('express');
-const cors = require('cors');
+const cors    = require('cors');
+var ejs = require('ejs');
 
 class Server {
 
     constructor() {
-        this.app  = express();
+        this.app = express();
         this.port = process.env.PORT;
+
+        this.app.set('views','../views');
+        this.app.set('view engine', 'ejs');
 
         this.server = require('http').createServer(this.app);
 
         this.paths = {
-
+            home: '/'
         }
 
 
         // Conectar a base de datos
-        this.conectarDB();
+        // this.conectarDB();
 
         // Middlewares
         this.middlewares();
@@ -25,32 +29,34 @@ class Server {
 
     }
 
-    async conectarDB() {
-        await dbConnection();
-    }
+    // async conectarDB() {
+    //     await dbConnection();
+    // }
 
 
     middlewares() {
 
+        this.app.use(express.urlencoded({ extended: false }));
+
         // CORS
-        this.app.use( cors() );
+        this.app.use(cors());
 
         // Lectura y parseo del body
-        this.app.use( express.json() );
+        this.app.use(express.json());
 
         // Directorio Público
-        this.app.use( express.static('public') );
+        this.app.use(express.static('../public'));
 
     }
 
     routes() {
-        
+        this.app.use(this.paths.home, require('./routes/home'));
     }
 
 
     listen() {
-        this.app.listen( this.port, () => {
-            console.log('Servidor corriendo en puerto', this.port );
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
         });
     }
 
